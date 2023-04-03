3023109a9a236c115548a34c18796782855f3dc4
tuuna <b7712983@naver.com>
2023-04-03 18:41:18 +0900

    마을 및 상점 구축 작업 진행중
    마을 및 상점 구축 작업 진행중

---
+++ src/ui/village/perry_village_popup.gd
@@ -0,0 +1,16 @@
+extends "res://src/ui/village_popup.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# 상점 클릭
+func _on_shop_npc_pressed():
+	pass # Replace with function body.
